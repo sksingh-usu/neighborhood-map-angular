3f684248e5fbfe7db456221ae408ee2c61bcc7c2
sumit <sksingh.mits@gmail.com>
2016-05-15 22:47:15 -0500

    clean the code

--- js/app.js
+++ js/app.js
@@ -1,17 +1,3 @@
-//
-// //Define an angular module for our app
-// var myApp = angular.module('myApp', ['ngRoute']);
-// myApp.config(['$routeProvider',
-//     function($routeProvider) {
-//         $routeProvider.
-//         when('/restaurants', {
-//             templateUrl: 'templates/restaurants.html',
-//             controller: 'restaurantController'
-//         }).
-//         otherwise({
-//             redirectTo: '/'
-//         });
-//     }]);
 
 var myApp = angular.module('myApp',['ngRoute'])
     .config(['$routeProvider', function($routeProvider){
